cdf043a1f2ff4a01bd5b54ca4f181edd85664f09
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-25 22:01:36 -0300

    :sparkles: Add poor side menu.

--- app/src/main/java/com/example/jourdanrodrigues/controk/MainActivity.java
+++ app/src/main/java/com/example/jourdanrodrigues/controk/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.jourdanrodrigues.controk;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] mMenuEntries = getResources().getStringArray(R.array.menu_entries);
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ListView mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+//        // Set the adapter for the list view
+//        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mMenuEntries));
+//        // Set the list's click listener
+//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 }
