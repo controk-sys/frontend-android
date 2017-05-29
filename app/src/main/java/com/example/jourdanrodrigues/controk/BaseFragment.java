881a73a815847c659b48a24e817de0c709d0f39d
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-05-29 20:39:31 -0300

    :snowflake: Improve fragment animations.

--- app/src/main/java/com/example/jourdanrodrigues/controk/BaseFragment.java
+++ app/src/main/java/com/example/jourdanrodrigues/controk/BaseFragment.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public abstract class BaseFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
@@ -20,6 +22,36 @@ public abstract class BaseFragment extends Fragment {
         View view = inflater.inflate(getFragment(), container, false);
         setTitle();
         return view;
+    }
+
+    @Override
+    public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
+        Animation animation = super.onCreateAnimation(transit, enter, nextAnim);
+
+        // Smoothing fragment animations: http://blog.danlew.net/2013/09/03/smoothing_performance_on_fragment_transitions/
+        if (animation == null && nextAnim != 0) {
+            animation = AnimationUtils.loadAnimation(getActivity(), nextAnim);
+        } else if (animation != null) {
+            final View view = getView();
+            assert view != null;
+
+            view.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+            animation.setAnimationListener(new Animation.AnimationListener() {
+                public void onAnimationEnd(Animation animation) {
+                    view.setLayerType(View.LAYER_TYPE_NONE, null);
+                }
+
+                @Override
+                public void onAnimationStart(Animation animation) {
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                }
+            });
+        }
+
+        return animation;
     }
 
     private void setTitle() {
